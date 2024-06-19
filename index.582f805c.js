0de6e428e2e597bfeaaadcd84e3e10316d242176
Valentyn Sobol <valiksobol14@gmail.com>
2024-06-19 21:20:16 +0300

    make build

---
+++ index.582f805c.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__large-img");document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault(),e.src=t.target.closest(".list-item__link").href});
+//# sourceMappingURL=index.582f805c.js.map
